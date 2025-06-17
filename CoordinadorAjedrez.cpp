cc329fbeb891620ffd23c06888962a74a753fec7
OmarFL <80384332+OmarFL@users.noreply.github.com>
2025-06-18 00:12:30 +0200

    Update CoordinadorAjedrez.cpp

--- CoordinadorAjedrez.cpp
+++ CoordinadorAjedrez.cpp
@@ -6,4 +6,22 @@ void coordinador::dibuja() {
 		0.0, 1.0, 0.0);      // orientación hacia el eje Y+ 
 
 //ESTADOS
+
+	switch(estado){
+		case START:
+			glEnable(GL_TEXTURE_2D);
+			glDisable(GL_LIGHTING);
+	
+			glBegin(GL_POLYGON);
+			glColor3f(1, 1, 1);
+			glTexCoord2d(0, 0); glVertex2d(-10, 45); //Esquina izquierda superior
+			glTexCoord2d(0, 1); glVertex2d(-10, -5); //Esquina izquierda inferior
+			glTexCoord2d(1, 1); glVertex2d(55, -5); //Esquina derecha inferior
+			glTexCoord2d(1, 0); glVertex2d(55, 45); //Esquina derecha superior
+			glEnd();
+	
+			glEnable(GL_LIGHTING);
+			glDisable(GL_TEXTURE_2D);
+			break;
+	}
 }
