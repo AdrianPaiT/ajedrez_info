601067790111a7d1a77aa7e0bf56ccaefe647770
OmarFL <80384332+OmarFL@users.noreply.github.com>
2025-06-18 00:05:59 +0200

    Update CoordinadorAjedrez.cpp

--- CoordinadorAjedrez.cpp
+++ CoordinadorAjedrez.cpp
@@ -1,3 +1,9 @@
 #include "CoordinadorAjedrez.h"
 
+void coordinador::dibuja() {
+	gluLookAt(15, 18, 48,    // ubicación inicial del ojo (perspectiva)
+		15, 18, 0,			 // punto hacia el que mira el ojo
+		0.0, 1.0, 0.0);      // orientación hacia el eje Y+ 
 
+//ESTADOS
+}
